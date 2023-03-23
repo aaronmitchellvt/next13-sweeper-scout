d5f32e9bf57961a561fe60d888450ad9881149b1
aaron mitchell <aaronmitchell9902@gmail.com>
2023-03-23 14:59:33 -0400

    install and configure tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main className="bg-orange-400">
+      <div className="">
         <h1>Hello World</h1>
       </div>
     </main>
